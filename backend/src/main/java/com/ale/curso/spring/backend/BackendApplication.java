72a4f1fb0a897b55f6e0af6e9c332f78c78081c7
Matheus Ale da Silva <m.matheus.ale@gmail.com>
2018-08-12 14:33:02 -0300

    configuração do banco de dados h2 e criação da entidade de dominio Categoria

--- backend/src/main/java/com/ale/curso/spring/backend/BackendApplication.java
+++ backend/src/main/java/com/ale/curso/spring/backend/BackendApplication.java
@@ -2,8 +2,12 @@ package com.ale.curso.spring.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EntityScan("com.ale.curso.spring.domain")
+@ComponentScan(basePackages={"com.ale.curso.spring.resources"})
 public class BackendApplication {
 
     public static void main(String[] args) {
